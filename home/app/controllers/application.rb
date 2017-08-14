d4512d4e93b9858bbe7efd5aeb017774360c8ea0
aishwaryakamath <aishwaryakamath.94@gmail.com>
2017-08-15 00:09:55 +0530

    Add files via upload

---
+++ home/app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'acc342d2e8fb4dd13919c171dda66269'
+end
